a7aca12558e0f71f1c66c23323b4c8baf8508e8c
GezerM <muzaffergezer@gmail.com>
2021-06-12 21:23:01 +0300

    eU 5 review work 2

--- src/test/java/Practice1.java
+++ src/test/java/Practice1.java
@@ -2,5 +2,7 @@ public class Practice1 {
 
     public static void main(String[] args) {
         System.out.println("EU5 is liking Git on IntelliJ");
+
+        System.out.println("WORK 1");
     }
 }
